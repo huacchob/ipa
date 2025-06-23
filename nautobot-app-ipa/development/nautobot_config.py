fc3d990f9e2b8c4800be74ddf298f41c9b71a4e5
Carlos Huaccho <huaccho.carlos0612@gmail.com>
2025-06-23 11:29:44 -0400

    adding meraki_managed platform dispatcher

--- nautobot-app-ipa/development/nautobot_config.py
+++ nautobot-app-ipa/development/nautobot_config.py
@@ -163,6 +163,7 @@ PLUGINS_CONFIG = {
             "netscaler": "netscaler_ext.plugins.tasks.dispatcher.netscaler_ext.NetScalerDriver",
             "cisco_nxos": "netscaler_ext.plugins.tasks.dispatcher.nxos_ext.NetmikoCiscoNxos",
             "cisco_meraki": "netscaler_ext.plugins.tasks.dispatcher.meraki_ext.MerakiDriver",
+            "meraki_managed": "netscaler_ext.plugins.tasks.dispatcher.meraki_ext.MerakiDriver",
         },
         # "default_deploy_status": "Not Approved",
         # "get_custom_compliance": "my.custom_compliance.func"
