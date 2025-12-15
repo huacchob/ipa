2085f0795df494914f3a64b400cbb2c7e75df2df
huacchob <carlos.huaccho@networktocode.com>
2025-12-15 09:11:24 -0500

    adding

--- ipa/__init__.py
+++ ipa/__init__.py
@@ -18,8 +18,8 @@ class IpaConfig(NautobotAppConfig):
     description = "Ipa."
     base_url = "ipa"
     required_settings = []
-    min_version = "2.4.2"
-    max_version = "2.9999.9999"
+    min_version = "3.0.2"
+    max_version = "3.9999.9999"
     default_settings = {}
     caching_config = {}
     docs_view_name = "plugins:ipa:docs"
